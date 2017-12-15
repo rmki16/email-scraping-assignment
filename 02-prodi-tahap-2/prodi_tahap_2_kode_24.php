e019d6d9743bd9bd4a7495729bfbd187603de988
Baha ID <coding@baha.web.id>
2017-12-15 15:05:17 +0700

    Init prodi_tahap_2_kode_24.php

---
+++ 02-prodi-tahap-2/prodi_tahap_2_kode_24.php
@@ -0,0 +1,106 @@
+<?php
+$html = array(
+"http://forlap.dikti.go.id/prodi/detail/RTQ4REEwRkMtMkZEMy00RkQxLTgxNEItMkUwQkM1NDg1MEU2/0",
+"http://forlap.dikti.go.id/prodi/detail/OEUzMERGNzgtOEI3OS00NjZBLTlEQUYtOTFEQTE1RTlDRkMz/0",
+"http://forlap.dikti.go.id/prodi/detail/RTBCMDYzQjctMzM2OC00QUY4LThBQjEtQkVEMjk0NDM1NDZB/0",
+"http://forlap.dikti.go.id/prodi/detail/QUNFRUY2QTUtODA0My00NzJDLTk0NkEtM0E3Q0E0NjE0MUI0/0",
+"http://forlap.dikti.go.id/prodi/detail/QkIzQjQ4MEQtRkVGNi00MEIyLUJEODMtOTZDN0IxNTE5RTJE/0",
+"http://forlap.dikti.go.id/prodi/detail/NjVEMjQ0NzktRDE1Ny00NkU1LTkyMTItQTlGQUQyRUYyRjJF/0",
+"http://forlap.dikti.go.id/prodi/detail/Mjg5ODIxNEMtMjk3OC00RTU0LTgxRjEtQzU0ODczRUNBNEVC/0",
+"http://forlap.dikti.go.id/prodi/detail/RkUwMDkzQkItREUwMS00ODdBLUFCNEUtOUU2OUM1ODI0N0Y2/0",
+"http://forlap.dikti.go.id/prodi/detail/MUE3QTgzOTctNDYzMC00NDEwLThFOUMtOEMyNTA4NDE0RjNE/0",
+"http://forlap.dikti.go.id/prodi/detail/MzhGQkM4MTUtMjQyRC00NEFBLUI1M0UtMjEyN0U5RjVFNUUz/0",
+"http://forlap.dikti.go.id/prodi/detail/Qzk4M0Q2NTktRkJDMi00MkU5LUJGMjItRTY1ODNFQUMxM0Iz/0",
+"http://forlap.dikti.go.id/prodi/detail/M0M4OEVGN0YtRkZFQS00RkQwLTk5NkQtOEE4QjEzMUNEQzA4/0",
+"http://forlap.dikti.go.id/prodi/detail/REJDMzI2QTgtQTExQy00NTU2LUIyQUEtOTJCQUY3NDZGNEY1/0",
+"http://forlap.dikti.go.id/prodi/detail/MURDM0M5QTEtNkQ2Ny00NTExLTkwNzUtMkIwRkNBODBEODlC/0",
+"http://forlap.dikti.go.id/prodi/detail/OUIyQjEwQjYtMzUwOS00N0M0LUFBN0ItMjU4NzUxREU3RTIx/0",
+"http://forlap.dikti.go.id/prodi/detail/NDJDQTU0MUQtMTZGMC00RDRBLTlFQjctRTlCMjIyREVBQTYw/0",
+"http://forlap.dikti.go.id/prodi/detail/QjRBMTU4QzEtMEZDQS00MjM4LUJGNUYtOTg4M0E1MUVFMTBE/0",
+"http://forlap.dikti.go.id/prodi/detail/NjM0MjY4NTktNDhCMC00QTVFLTg0NzMtRjY4NTMzRjY4NDE1/0",
+"http://forlap.dikti.go.id/prodi/detail/NkI3MzBFRjgtMzQ3NC00MkVDLUE3MDEtNEYwRDU2Mzc2MkYz/0",
+"http://forlap.dikti.go.id/prodi/detail/MjQyRUY5OTktOTgyNS00RDg2LUI3QjgtOTVBM0U2RUVCOUI1/0",
+"http://forlap.dikti.go.id/prodi/detail/NDRDOThBMzItNTlBQS00QzJCLUI3MUQtNTgyQjFFQkM0RUUw/0",
+"http://forlap.dikti.go.id/prodi/detail/Q0ZGRDNDRDYtN0Y0QS00MTA0LUI3RTgtMzE4RkRGMTE0Qjk5/0",
+"http://forlap.dikti.go.id/prodi/detail/QTRCRUVFNTctMzQ5Ri00ODI5LUEzRjctQkY5OTdCMjI1M0ND/0",
+"http://forlap.dikti.go.id/prodi/detail/RDQzNzYxMDMtNkI0MS00RjhDLTg5MjgtMkZERUVDMDhGRjY5/0",
+"http://forlap.dikti.go.id/prodi/detail/QUZCMzM4OUQtNzYwMy00RTkxLUIxMUUtOUM2MzcwM0I0NzZD/0",
+"http://forlap.dikti.go.id/prodi/detail/OUI1NkM3NDItMTJBQS00QjJELUFFRDYtNkM0QUQ4NzVGQjc0/0",
+"http://forlap.dikti.go.id/prodi/detail/MzYwNkI0MTgtNjVDRi00M0UwLUE1RkUtMENBNUU4Q0ExM0JB/0",
+"http://forlap.dikti.go.id/prodi/detail/MjQ5RDE0NEEtNEJERS00MzNFLUE5RDktRDRDMkIyQUQ2MjFG/0",
+"http://forlap.dikti.go.id/prodi/detail/QURBMDU5QTMtNkUwRS00NkY2LThBNTUtNkRGNDlEQjk4QUY5/0",
+"http://forlap.dikti.go.id/prodi/detail/NzM5QjU4OTItNzM3RC00Qjk0LUJDMzItQkFDMkY3Q0ExNjE0/0",
+"http://forlap.dikti.go.id/prodi/detail/MUQ0RDA1QjQtNDJFMS00NEE0LTg4NkItQzg4OUIwNDEyQjVG/0",
+"http://forlap.dikti.go.id/prodi/detail/MUNERkNBMDktM0JFMS00QUQ2LTg4NUMtMzU4MzQ3MTY5NjlB/0",
+"http://forlap.dikti.go.id/prodi/detail/NUI4MkQ1MUItQTc3NC00MUZELTlERUQtOEYxQ0NDQzQxMzA5/0",
+"http://forlap.dikti.go.id/prodi/detail/MENENzRBNzItODA0Ny00MzU3LUFDMzYtODJDQUY1N0Q5QTM4/0",
+"http://forlap.dikti.go.id/prodi/detail/REM0QUYyNDUtRjI3OS00RTU2LUFCQkItQjE3Q0YyNzlGQjAw/0",
+"http://forlap.dikti.go.id/prodi/detail/QTVENTdCM0YtMkFGNS00NDk4LUI0NzItMjk0MTU4NjFFN0Yw/0"
+        );
+
+
+function bacaHTML($url)
+{
+    // inisialisasi CURL
+    $data = curl_init();
+    // setting CURL
+    curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($data, CURLOPT_URL, $url);
+    // menjalankan CURL untuk membaca isi file
+    $hasil = curl_exec($data);
+    curl_close($data);
+    return $hasil;
+}
+
+date_default_timezone_set("Asia/Jakarta");
+echo "=========================<br>";
+echo ">> Mulai : pukul " . date('h:i:s') . " WIB<br>";
+echo "=========================<br>";
+
+echo "No : Nama PT : Kode Program Studi : Nama Program Studi : Alamat : Kode Pos : Telepon : Faximile : Email<br>";
+
+for($x=0; $x<=35; $x++)
+{
+    $kodeHTML = bacaHTML($html[$x]);
+
+    $namaPTN = explode("<td class='tright' valign='top'>Perguruan Tinggi</td>", $kodeHTML);
+    $namaPTN1 = explode("<td class='tright' valign='top'>Kode Program Studi</td>", $namaPTN[1]);
+
+    $kodeprodi = explode("<td class='tright' valign='top'>Kode Program Studi</td>", $kodeHTML);
+    $kodeprodi1 = explode("<td class='tright' valign='top' width='200px'>Nama Program Studi</td>", $kodeprodi[1]);
+
+    $namaprodi = explode("<td class='tright' valign='top' width='200px'>Nama Program Studi</td>", $kodeHTML);
+    $namaprodi1 = explode("<td class='tright' valign='top'>Tanggal Berdiri</td>", $namaprodi[1]);
+
+    $alamatPTN = explode("<td class='tright' valign='top'>Alamat</td>",$kodeHTML);
+    $alamatPTN1 = explode("<td class='tright' valign='top'>Kode Pos</td>",$alamatPTN[1]);
+
+    $kodeposPTN = explode("<td class='tright' valign='top'>Kode Pos</td>",$kodeHTML);
+    $kodeposPTN1 = explode("<td class='tright' valign='top'>Telepon</td>",$kodeposPTN[1]);
+
+    $teleponPTN = explode("<td class='tright' valign='top'>Telepon</td>",$kodeHTML);
+    $teleponPTN1 = explode("<td class='tright' valign='top'>Faximile</td>",$teleponPTN[1]);
+
+    $faxPTN = explode("<td class='tright' valign='top'>Faximile</td>",$kodeHTML);
+    $faxPTN1 = explode("<td class='tright' valign='top'>Email</td>",$faxPTN[1]);
+
+    $emailPTN= explode("<td class='tright' valign='top'>Email</td>",$kodeHTML);
+    $emailPTN1= explode("<td class='tright' valign='top'>Website</td>",$emailPTN[1]);
+
+	echo $x+1;
+    echo $namaPTN1[0];
+    echo $kodeprodi1[0];
+    echo $namaprodi1[0];
+    echo $alamatPTN1[0];
+    echo $kodeposPTN1[0];
+    echo $teleponPTN1[0];
+    echo $faxPTN1[0];
+    echo $emailPTN1[0];
+    echo "<br>";
+}
+
+echo "==========================<br>";
+echo ">> Selesai : pukul " . date('h:i:s') . " WIB<br>";
+echo "==========================<br>";
+
+?>
