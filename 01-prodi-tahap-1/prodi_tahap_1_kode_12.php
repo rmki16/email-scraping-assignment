be8d235351028d51acfb2a5ed02258cc68d369d6
Baha ID <coding@baha.web.id>
2017-12-15 14:59:51 +0700

    Init prodi_tahap_1_kode_12.php

---
+++ 01-prodi-tahap-1/prodi_tahap_1_kode_12.php
@@ -0,0 +1,87 @@
+<?php
+$prodi = array(
+"http://forlap.dikti.go.id/perguruantinggi/detail/MENBNjkzMjAtQTYxRS00OTZCLTkzMzYtMDMzNzA4NzZFMDNG",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MTlDMUZFQTgtODhCRC00MjkyLUE1NjctRDc2RjgzRDVENUZE",
+"http://forlap.dikti.go.id/perguruantinggi/detail/Mjg2RjBFMDYtOTQ5RC00ODdELTkzMDYtNThBQTFGNURBRDhC",
+"http://forlap.dikti.go.id/perguruantinggi/detail/Mjk0MkRCQzEtNDJBQS00M0VFLUJDOUQtRkZEM0Q3MDU4MzMz",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MzgxRjM2NjgtNTRFOS00RDE4LUIyQ0QtOEZDMkNDQ0U5ODNF",
+"http://forlap.dikti.go.id/perguruantinggi/detail/N0I5NzFEMjctMTM1OC00MkE3LTlGQjMtREVEQzJERjlCNDI3",
+"http://forlap.dikti.go.id/perguruantinggi/detail/NEMzQTlGQUYtN0Q4QS00MzU5LUE3MTgtQTQzRDU5OEZENDJD",
+"http://forlap.dikti.go.id/perguruantinggi/detail/NENGM0RCRjktMzA4MS00NzgzLTkwNzMtRUY0MTFFMkRGQzU2",
+"http://forlap.dikti.go.id/perguruantinggi/detail/NzhGRDE0QzktRTE4NC00RUVDLUFDQ0EtRTJBN0EwMENERjJC",
+"http://forlap.dikti.go.id/perguruantinggi/detail/ODJDMjJBM0EtOUY5Ni00QTIwLThBQTItMjEzNTI3NkJDNzAy",
+"http://forlap.dikti.go.id/perguruantinggi/detail/ODUyMDIwNTEtQTQ0RS00QUE2LUIxREMtNDc1QzQzMjBGNEIw",
+"http://forlap.dikti.go.id/perguruantinggi/detail/OERDRTk3NjAtNjk1NC00MkI5LTk1MzMtNDY1Q0FDMzEyQUU2",
+"http://forlap.dikti.go.id/perguruantinggi/detail/OTVDRjQ5NDEtQjhFQy00QzcxLUI5OEUtOEE1NkI5OTJBNEQx",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QTdEQzdBRTgtMjQxRC00MjkxLTk3QjYtMUJBQTFCNzIwMzMy",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QjFDQTU3ODUtRTkyOC00NkVBLUE5QTAtREFFRDk3RUJCQzY4",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QkJEOTE3OTQtRkExNy00OUU2LTg4MkQtQ0NDNUMzRDQxMEY1",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QzIxN0JCQUEtRUNGMi00NzA2LUI0N0QtQzNERjg2QTBDNDgw",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QzY2QkJGODktNTI5OS00RDRGLUE4REUtMkQxNjk3Q0ExRTM5",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RDc5RkYwQTgtRDFERC00MUY0LThBN0MtMkZCQjVEQjNCNjhD",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RTFBN0U5RjYtQzREQi00QkRBLTg2QzItQ0NDMjRGNDhCMTU1",
+"http://forlap.dikti.go.id/perguruantinggi/detail/M0I3NDAyODUtRThDMy00RUVCLUIzRTEtRERFNERCREYzMjFD",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MDA5QjVDNEQtRDU2Qi00ODlFLTk5MjAtNUJGNzM3RjRGOEFD",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MDVENkNCMTgtNkMxMC00NjlDLTkwNkMtMDE5QzA4MjIwM0Ez",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MTBBOTJBN0MtNjhCMi00RDI0LUJBQjctRDlFREI1MjRBOTIx",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MUY3MjU2NjktNTYxQy00MzFELTlEQkItOEQ5ODJCQ0JBNjZG",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MjA3QUFDREMtMkJBRS00Qjg4LUFEM0MtRDcwM0JFNjgwMzVG",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MkI3RkJDQ0MtMURBMy00NEZDLUIyNDAtODZBQUQwRUVGOUVG",
+"http://forlap.dikti.go.id/perguruantinggi/detail/NEQxRTgyQUQtODQzQy00OTQ5LUFGQzMtNTA2QzY4MDQ0QjFE",
+"http://forlap.dikti.go.id/perguruantinggi/detail/NEZDMzI1RjYtNzBCNy00NUExLUE2NjAtNkEyRTQzNzU4NjU5",
+"http://forlap.dikti.go.id/perguruantinggi/detail/OUNCMjM0ODQtQkZGMi00RUQ4LUI3MzMtMjA5MkEzNTcxRTlF",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QTFFQURFMzYtM0IwMi00QzgwLUFGMDMtODQ1RjM2MEZERkNC",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QUFDRTdFRjctRjdDOS00MUYyLUIyNTItQTZEMkZFQjJDRjVE",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QjQwMEVFRDktRTEyNi00QjI2LTkyRjEtQjc3QTk2NEU3OEFF",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QkM1NUM5RDgtMTM3OC00Rjg1LUE3RjgtNkM5MTg4QkFGMURB",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QzNBMjBBOEUtNDNCQi00MDFCLTk5NjMtNkVFNjA4QkNDMUNB",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RDg1NTYzMzQtMDMzOC00MUM3LTgzMEEtRDA1QkU1REQ1RDE5",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RTM4QTc2MDUtQkEzNy00QjcyLTlGNTktRTMyRTBFOUZEMDk3",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RTg0QjA1NDktMDY0NC00RTM2LThGM0YtQkMzMkM0NjhGNkJD",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RThBNjQ3OTYtRkEzRS00OUMwLUFCOUItMzhBMkIyQTEwNDE2",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RUM5NTc3QjgtMUM1Ni00RjYwLThBODAtOERBNkVGODIxODRB",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MDc4NjU2QjEtNUFENi00OUE4LUI2MEItNUJGNzdFNDBCMzNG",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MUFFMzYxODQtMzk0Qy00MUFGLUI3QkUtOTZBRkU5MDQ1NjZG",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MjQ0QUMwNzktNTE2Mi00QjVFLThFNjEtREVBNkFFNjA5QTQ1",
+"http://forlap.dikti.go.id/perguruantinggi/detail/MkY2NDVCOUYtMkVFNy00MkUzLUJCOTEtQzc4MjcyNjlBNUE2",
+"http://forlap.dikti.go.id/perguruantinggi/detail/OTgyNDU2MkMtMjc0Qy00NDU0LUI3NEEtRTdFRDI4M0Q3NkRC",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QTM3Q0YzMDQtRkQ5MS00OEMxLTgwOTUtN0MyN0NFRUQwN0M3",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QTY4MTM0ODItNUY5NC00NEUyLTlBQkMtOUY1MDBDMTIxM0Yx",
+"http://forlap.dikti.go.id/perguruantinggi/detail/QUVFNjA2REYtRjhGMi00RUU1LUFGRTgtQjUzNDFENTk0NjBB",
+"http://forlap.dikti.go.id/perguruantinggi/detail/RkNCQTI1MjYtMjA3Qi00QjUzLUI5NzItNTlCM0VEQjBCRDc2",
+			);
+
+date_default_timezone_set("Asia/Jakarta");
+echo "=========================<br>";
+echo ">> Mulai : pukul " . date('h:i:s') . " WIB<br>";
+echo "=========================<br>";		 
+
+for($x=0; $x<=48; $x++)
+{
+    $html = file_get_contents($prodi[$x]);
+    //Create a new DOM document
+    $dom = new DOMDocument;
+
+    //Parse the HTML. The @ is used to suppress any parsing errors
+    //that will be thrown if the $html string isn't valid XHTML.
+    @$dom->loadHTML($html);
+
+    //Get all links. You could also use any other tag name here,
+    //like 'img' or 'table', to extract other tags.
+    $links = $dom->getElementsByTagName('a');
+
+    //Iterate over the extracted links and display their URLs
+    foreach ($links as $link)
+    {
+        //Extract and show the "href" attribute.
+        //echo $link->nodeValue;
+        echo $link->getAttribute('href'), '<br>';
+    }
+}
+
+echo "==========================<br>";
+echo ">> Selesai : pukul " . date('h:i:s') . " WIB<br>";
+echo "==========================<br>";
+
+?>
+
