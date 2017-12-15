78ab6b082d1d9d3ae0c8a603e0e53cc7f4e4e07f
Baha ID <coding@baha.web.id>
2017-12-15 15:08:37 +0700

    Init pt_kode_05.php

---
+++ 03-pt/pt_kode_05.php
@@ -0,0 +1,193 @@
+<?php
+$html= array(
+           "http://forlap.dikti.go.id/perguruantinggi/detail/M0VDNDM0QUQtREQ3Ny00NENGLUFBMzgtNURCNTFGNDM4MjQy",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/M0VEMENBODEtMDM3NC00RDA5LTlFRUYtODM5Nzk5MzU1QjE3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MzYxNkI3NjMtQzc0QS00MkY0LTg4QzQtRDk0RDFBQzcxOUI1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Mzg1RUJDREEtQjg4OS00MjY5LUI2QzYtREMwOTY3QzI5NkI4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/N0VBQUMxQzktRDc0Ny00ODMzLTk0NEItNEMyNjJGMUUxMzI2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NDZBQzQ5QjQtNjkwRi00NDIzLThDNEUtMUQ1OTRCRERGMjNF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NEI2RDBFRDktMTQxRS00NDg1LTkzQTYtQTQ0RUI3MzEyMzk3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NjRDNTFFRDktNjYzRi00MjlBLThGMjMtNzExRTJGN0I1M0RG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NkE5QkY5NzMtQzk4MC00N0NFLUJERDAtNTg2MDlGNzU3QjMy",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NkUyNERFMzAtMjJFNS00RjE4LTk1NTUtQTVGNEQ5QTNBNjQ4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/ODU1OTdEQTYtOUNERC00NEVGLUEyQTQtRjhDQzUxMUFDNDRB",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/ODcyMEY5MkMtREQyMi00RDU3LUI3MEItNTZDNzJFNUVGREMw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OEU1NzZENTktQTUyOS00MzgyLUIxMDEtMEU1RTM2MjZBRDcz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTFEODg5RjItODY1My00REVELUJFRDgtODRDMUI0QTExRTQ1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTQyNzMzMDUtODM0Qi00ODZFLUE3OTYtNzhDRThDOEZBRjJE",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Q0I0RTIzN0EtQUM3OS00MDZBLTg4MEMtRkY1MTlDMTNFODlC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QjU5MEY4OEYtMkUzRi00Q0NFLTlDNTktQ0NEN0MzMzk5QjA3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RDQ3N0RDREEtODAwNy00OTU3LTgyNjAtQTlGOEE2NjM3QkFC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RUY5ODMwNTItNDhBMC00QUZELUFBNEMtRkMxNDJCNTNGMEZG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RjkxN0RCRDQtNzBCOS00ODJELTkyQ0UtQkRCQTU2QjIwQ0RE",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MEVDNTRFNjEtREM3MC00OTYxLTgxRjAtMzkzREMxREVCQjJD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MzREMzRGQzktMjU3Qi00MjE1LUE5NTItNDYxMDAxNDRERENF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/N0MxOTYxRjItQjY1OC00N0VCLUFEOTMtOTEyRTE0MzQzN0Y1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NEM3QjAxRDktNjQ2Ny00MjM3LTlFRkYtMjYwRUJBQjUzRjZC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NTY4NDhBQTktNDU0RS00RTVGLTg4QUMtMjg0RENDOENEOTQ2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NjlEMTAxRDEtN0FBMi00RTc0LUI2RTktN0M2MEVBNjM4Njk1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/ODE2MkQ3MkItMUE4Ny00NjMzLTg5QkYtMzU4Q0M1QUI4MzZE",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTI1RTIxOUItRDU0MC00MjY1LUI2OUMtOEYzRTUzQjBDNTYx",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTk0NkI3RjQtNkFEMi00RjE2LUE5M0QtNzREMzU0Q0Y0NkFG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QTc4RkRFREMtREYwNS00QkZELUFBOTAtN0ZDMTJCMThCNzVC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QURGNTQzRTktMzY3MS00NEE2LTlBNTQtRURDNzNCNzRGMTY4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzA5MUJERUEtMjY3QS00QTc2LThGRjAtRjQ1NEREMTcwNDk4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzE0RjM3MDEtQTQ1NC00NDBDLUI4QjAtNDVERThFRjM3N0E2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/REFENUUxQ0EtREQ2RS00MzRDLTkyNzktMzZCQjhDN0U1QThG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RTI0RkVCNTQtODk4Mi00N0UzLTlBMEItMDVDN0QwMTE4RkE0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RTZFRTcxRDEtNjY5NC00NDI1LTg3RDYtNEZFNTFERkQ3NTI4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RUI1MkU0QzUtQUUyMi00OTc0LTkyN0YtNUY2MzZERTM2NTFE",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RUM2MjlCMkYtRDZEQS00QzQxLUEyNDQtNEYyRjUxMkJCMTZF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RjQyNDU3QjEtMzgwRi00NTRCLTkyODUtQkVDRUU3N0QxN0ND",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RkE3Njc4OEEtRDM0MC00NjU4LUJCNkMtM0I0MDNGOTI2MUY3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MEU0OUNBQzgtNjk1RC00QTgxLUFDNDUtOUM3NTFDNkM0NTkw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MTFEOUYzMEQtMDc0NC00NDY2LTg0REMtMjc5M0I1QTYxOEJF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MTQ3MkRGQjctNzExQi00RDVCLUE1N0YtOTkwNUY2NEQ4N0VD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MUM2NjM3NEEtNUQ5Mi00NzEzLThDOTYtNjAyN0U2REQzNTc1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MjA3MjJBQzEtQTAyQS00ODExLUI1QzYtRTEwNzJENjY5ODEw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MjAzQjEzQ0YtNzQyRC00QTk2LUEyNDYtODg2Q0YyODE0OTNG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MjBDMUVCQjYtNkUyNi00REU5LTk2QjAtQUFEMDVGRjE4MDAx",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MkU3OUI3OTQtMTIxOC00QUE3LTg5OEItOUY1NDVGRkY5RkRF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MzExNUJBMUMtMjM0Ni00NkExLUEwMUEtNTcwMTY3REVERDM0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MzNDQjQwRjYtNjNFMi00OEI1LUJCQkEtRjkwQ0YyMEFDOEQz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NDA0QUU0NUYtMkU3Qi00ODgxLTlBQTAtMDI2OUY0OTRERkIy",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NEQzMUM2Q0QtMkU4RS00MTQ0LUI5NTQtQ0I5NUI2RUJENUNF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NUIyNTJGREUtQjJENi00QzkzLTk0OEItMDAxQjQ3MEZCQjZE",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OUQ4MDRBRTAtMzNBMi00MTc0LTkzQzgtQUQ4NjhBMTRCRTZF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QUY5RjQ5RDAtQzVERi00NTNFLTlFNjctNzg1RDUzOUEzMzk1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Qjc0MzNGMEQtNjNENC00MzZGLUExNzgtQkJBRkQ2RTJDMUE4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzgwMDVGNTUtNTU1Ny00OTRDLUEzMUUtMDE3OTMwMzY3MkRC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RDg5RjlENTQtQjk0RS00MTM4LUFEQ0UtMDVGNDMwOUY5MDc0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Rjk1NUNGQkQtNjgwOC00QkRFLThDQzYtM0Y3MDQ4NkIxMEJF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Rjk5NjhCQjgtMDhGMC00RDIyLUIxRDUtOENCNkIzQjE4OTY0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MDA3NDBCNTktQjY1Mi00Mzg0LTk0NDUtQjdGMjBFNEMxOTMx",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MUZDMDM2MDMtRUM2QS00NzEzLUIzQzAtODZGMUNCNTVGRDY0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MkQ2RDc1RTctQjU4OS00NkNDLThBOTItMTFENUQwNDlCMzdE",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MzA2Q0ZENkYtRTQzOS00MjRCLUFCNDUtMkQ5RkM4OTAwNkM5",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MzgzNTY5NEEtNzRCQy00QzhELUI4RDItRjg1QTVGNDhFNTcw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NTI0RDQ2OEMtNDcxOS00MUNELTlDM0ItM0Y3MkRBODk1OUU4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NUQzMTRFNEUtOEE1Ni00Qjc1LUIwNkEtNDlBRTc0NDc4NUE4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NjA1NEVCOTItNkE2NS00MzAzLTlFMjktNzg0QjRGQ0ExOEU4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NzE1MjEyRjYtQUIwQi00NDdFLTgyQjYtMjU2REU3MzBCRjYw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QUM4NjJDN0EtMDIwOC00REUxLUE2RDYtNTMwNTNBQzQzMjU2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QUZFMzNDRjgtRjNDNy00QjlBLUIyRjktQUVCNTMyODIyRjNG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QjIxMTI5NjQtQjJCMy00MTBFLUExMTktOEFENjkyMzk2RDU0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QkIxNDQ3NUQtRDEwNy00MzlCLUFGMTctMDE3REUwODc4MUM2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QkY4NzI3MTMtNjEzNS00RDdCLTg5RjctNkMxMDhFQTkyNzM3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzE1MUFGMUMtRTk1NS00QTA1LThFMUItNkMxMDU4MkI2MEY3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzU2ODJFOUEtQzg4MC00QjRCLUE1NDQtMTAyNTNCOEI1REJD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzY0RDI1QUEtNTZGNC00ODczLUI0QkItNDg1RDM5NTFCMjQ3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RDgwNjAzRjgtNjAzRi00NEJGLTg5MzItQTFCMEZGMEFFNDVC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RTJBQkE3OEMtRTg1MS00QUIzLThBOTEtMUQxNjRDNTIzQTY2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RjYyOUM4M0YtMkM5Ny00MTgwLUJBQ0QtRUVDMkY3NkIxRjE5",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/M0YzMzRDNjUtREUzNC00NTk4LUI4N0YtREJCMzQxQzA1RTcw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MjdDMEMyRkQtQzIyRS00MjAxLUE2ODItOUU4NDIyNjgwREQy",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MzUxQzJERjItMjU3Qy00MDYwLUJDNzgtODlDN0YyNzYxMkJB",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NTY3NjZCMzAtNDE2Ny00MEYxLUJDMTAtRTBEOEVCMEI0RTYz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NUYwMkZFNzgtRUJBNi00N0NFLTg4NEItOUExNDNCMkUxMUM5",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NkIxNEJBQTYtRTNBRS00ODBBLUIxQ0QtQzFFRTU5QTYxMzhF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NkRDRDA1MzItRkZDNy00MkMzLTg1QTgtRUMxRjVDNzUxQjJG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NzE3REMwMzEtRjNFRi00QzE2LTlFQTAtQkQwMERDQzAxNzlC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/ODU5RjNDODMtNEYzOS00MzU0LUEzRjItMzEyRjdCOTY4NDBG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/ODk4NTc1MDctMkNEMy00QUVFLUE2RDItRTUzMjg5QzgwMURB",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Q0RFMDhFMzktRTZBNi00MUFELTlEQjEtMkM5NzgzODdBQjA2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QTA5RTdCMTItOUE4NC00QjA3LTlEMkQtNEQ0NUUzNTYyNEQ5",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QTE5NEI4N0QtRjM2MS00NUE2LUI0MzEtNTFCOEZBRjdGRTM1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QTk4OTZEMzktNTEwRS00ODdELTk5MUEtRjgyMjA0ODM5QkQx",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QUIzMTM5ODAtNUNDQy00ODlGLUIyODQtNzJGQjdFMEY2NzE5",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QjQ5QzUyRTUtNDNENC00MzZBLUIwNDMtQkMzQzc0OTlEOUJG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzY1QTY4MjEtOTY2My00RDVDLUE3MDktMTRCNzAyQjM1Qjk1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/REY0N0Q1RDItMTNCMy00OUIzLTkwREYtMDNGQkMxNUQzQTc4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RjIxOTMwMEUtRTUzRS00NTBBLTkxNUItRDAyOEZDQTQyN0JC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RkU5REM3MkUtRkMxRS00NkMyLTkzNDktNjUzMEU3RTAxRkUz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MEREMTc2RTAtMUZDOC00RkEzLTlGQTQtNjZFNjY1QTY2MjM0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MTRFMzREMjctNzg2MS00QUY3LTk5ODMtNjIyNDBBNzE4NUJD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NDJCRUMyM0YtNzgyRi00REJGLTgzNjctRTlBN0IxNEU1QkYz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NEYzNTY3N0EtREQxMS00NTU0LUI3MUQtMUYzQTM2Q0IzRTE2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NjMyMUNDQUYtQjcxMS00RUI3LThFNDMtMEY2REIxRDE0RjUy",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NjQ5MTlCMUEtMjE1My00MTNDLUE4RjctMjlENzdFRUEyNDhC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NzJCNTY2MTAtOTAwRS00ODNBLTkzNUYtQzUyRDc4RjM3Q0JD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NzRERDNDQUEtNTRBNy00MEVGLUI5NjAtQ0I4RjlCQ0Q1QUQ3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NzUxQTYxRDktQzQ0Qy00NDY2LTg4OTktNzIyM0Q2RTIxNDcw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NzkyMEQ2QjUtRUIwMi00RUI4LUFDRjMtQjA1RjA0NzVGNTRF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTdDQ0RCNjEtMTNGMC00ODk0LUI0OUMtNjUzMURCMzZCODgx",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTkxQTE4NkEtMzk1My00REU5LThFM0EtRDE4RjVCRkJGNTMw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Q0U4MzE2QTUtMzg2RS00MDIzLUJGQjQtNkQ5NDg1OTA0NUVD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QUM1QTJEMkMtMjFCQS00ODc0LUFBNDEtNDZCQ0M1RTZBQUY4",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QUY2RTZBMkYtMURFNy00QTQyLUI2NTItRDQwRjcxODY3QTlD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzgyNTlBQ0QtN0FDRi00RjY5LTlCNTgtOUYxQ0U4RUY2M0Ux",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RDdFNjdGMUQtOURCQy00QkRCLUJGMjktQkRDNzk2MDkzNjFG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RERERUMzN0UtQkY3My00MTEyLThGQTAtMkYyMUExNDE4QTM0",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RTg2N0ZDNTMtOERGNS00NUEwLUExNjQtQTUyM0UxRTVFQUQz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RjgwRUVGNzUtRUQ3Ny00NDgwLUJGODYtMTAxOTM5NUNDQTE3",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/MTAxMUExQjMtQzk2My00MjI0LTlEQkYtRTY2REZCRDcyQTQ2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/Mzc2QUE2REEtMkIxMS00RDM3LUJBRjktMDc2MzZBNUM3QTE1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NjQzMTMxNTgtMzUyRi00NkVFLTlEMjUtQ0UxMDVBRjIxN0JF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NkE2NjhGQkEtNjIxRS00NjhDLTkyNjItQTNBNTFBRDRGQ0Iw",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/NzU0NjZGODYtOUJDQi00NzEzLUIyQ0UtQTE1QTZEMzY5QkRC",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/ODY0MjRDQkYtMTE3Qi00Mjg2LThDRTYtMjgzMjMyREY4Rjc1",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OEIxRTBCRTktNjkyRS00MTIxLTkxNUYtQjYwMkE1RkE3QUVF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OEJERTJFNUMtOUZBMi00REE4LUI4NjYtQ0Q1RUFGRkYzRDQz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTFDMDIyQTAtODAyNS00ODlFLUJFN0UtNjVEQUQzQTA1Qzcz",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/OTRCNTkzQkEtMTQ0Qi00REQ1LTg4NEItQzMzREI5Q0FDMjBD",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QjEyNjdEQTctQ0NCNy00ODEwLTlFOUEtQzk2RkIyQkFBMUY2",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzAxQjgxQkItMEE1Qi00M0JCLThEODktMUFFMkQ1OTkzRTgx",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/QzM5RjM2ODktRDQyMi00NEUzLUI2ODItNjNGMzcwODcwM0Yx",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/REZGNkUwRkQtMzlDQy00Q0UzLUI1RjgtN0ZBOTY3OEEyMTlF",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RTRFRjU3MTYtQzdDQy00NzY2LUI4N0EtMDlEMzRGMjczRjdB",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RjE4MTlDNDgtREU1RC00MDNELUIyRDgtMkQ1MjhBQzU1OERG",
+           "http://forlap.dikti.go.id/perguruantinggi/detail/RjJEODg5OTAtODIzMC00RDhFLUI5NDAtNEFEOEYzODUzNTgw"
+       );
+function bacaHTML($url)
+{
+    // inisialisasi CURL
+    $data = curl_init();
+    // setting CURL
+    curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($data, CURLOPT_URL, $url);
+    // menjalankan CURL untuk membaca isi file
+    $hasil = curl_exec($data);
+    curl_close($data);
+    return $hasil;
+}
+
+echo "No : Nama PT : Alamat : Kota/Kabupaten : Kode Pos : Telepon : Faximile : Email<br>";
+
+for($x=0; $x<=136; $x++)
+{
+    $kodeHTML = bacaHTML($html[$x]);
+
+    $namaPTN = explode("<td class='tright'>Perguruan Tinggi</td>", $kodeHTML);
+    $namaPTN1 = explode("<td class='tright'>Tanggal Berdiri</td>", $namaPTN[1]);
+
+    $alamatPTN = explode("<td class='tright'>Alamat</td>",$kodeHTML);
+    $alamatPTN1 = explode("<td class='tright'>Kota/Kabupaten</td>",$alamatPTN[1]);
+
+    $kotaPTN = explode("<td class='tright'>Kota/Kabupaten</td>",$kodeHTML);
+    $kotaPTN1 = explode("<td class='tright'>Kode Pos</td>",$kotaPTN[1]);
+
+    $kodeposPTN = explode("<td class='tright'>Kode Pos</td>",$kodeHTML);
+    $kodeposPTN1 = explode("<td class='tright'>Telepon</td>",$kodeposPTN[1]);
+
+    $teleponPTN = explode("<td class='tright'>Telepon</td>",$kodeHTML);
+    $teleponPTN1 = explode("<td class='tright'>Faximile</td>",$teleponPTN[1]);
+
+    $faxPTN = explode("<td class='tright'>Faximile</td>",$kodeHTML);
+    $faxPTN1 = explode("<td class='tright'>Email</td>",$faxPTN[1]);
+
+    $emailPTN= explode("<td class='tright'>Email</td>",$kodeHTML);
+    $emailPTN1= explode("<td class='tright'>Website</td>",$emailPTN[1]);
+
+    echo $x+1;
+    echo $namaPTN1[0];
+    echo $alamatPTN1[0];
+    echo $kotaPTN1[0];
+    echo $kodeposPTN1[0];
+    echo $teleponPTN1[0];
+    echo $faxPTN1[0];
+    echo $emailPTN1[0];
+    echo "<br>";
+
+}
+
+?>
